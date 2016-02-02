f572a440d66c6a5775b1f74ed2283e5a58550710
“michaelmcqueen” <michael@variantstudios.com>
2016-02-02 14:00:33 -0700

    fixed mobile menu and header and preface regions on mobile

--- sites/all/themes/novel/assets/js/scripts.js
+++ sites/all/themes/novel/assets/js/scripts.js
@@ -1,11 +1,12 @@
 (function ($) {
   $(document).ready(function() {
     
-  $('.js nav').prepend('<button class="menu-btn">Menu</button>');
+  $('.js nav').prepend('<div class="menu-btn"><div id="nav-icon3"><span></span><span></span><span></span><span></span></div><div class="menu-label">Menu</div></div>');
   $('.js nav ul').addClass('hide');
   $(".menu-btn").click(function(){
         $(".js nav ul").toggleClass('hide');
         $(".menu-btn").toggleClass('close');
+        $("#nav-icon3").toggleClass('open');
     });
   
   if(Modernizr.mq('only all')) {
